06ebaa71c2c6e1417d0d7701f27664960221ede5
LautaroCutri <lautarocutri@hotmail.com>
2017-05-10 23:22:06 -0300

    Transition to functions & Cesar encryption

--- main.cpp
+++ main.cpp
@@ -17,11 +17,12 @@ void Busysleep(int decisegundos){ // cross-platform busy-sleep function
     while (clock() < time_end)
     {
     }
+    return;
 }
 
-void Clock_deco(){ //clock decoration
+void Clock_deco(int delayClock){ //clock decoration
 
-        for (int ru=0; ru<2; ru++)
+        for (;delayClock<10; delayClock++)
         {
             cout << " |";cout.flush();bs(7);
             cout << '\b' << '\b';
@@ -30,12 +31,24 @@ void Clock_deco(){ //clock decoration
             cout << " \\";cout.flush();bs(7);
             cout << '\b' << '\b';
         }
+        return;
+}
+
+int Temperatura(void){
+float temperaturaF, temperaturaC;
+    cin >> temperaturaF;
+    temperaturaC = 5.0/9 * (temperaturaF - 32);
+    return temperaturaC;
 }
 
 int main()
 {
     int programa;
+    string fecha;
+    string compare;
     string nombre;
+    string toCode;
+    string::size_type desfasaje;
 //Just a cool welcome effect
 {
     cout << "H";cout.flush();bs(3.5);
@@ -48,9 +61,8 @@ int main()
     cout << ".";cout.flush();bs(3.5);
     cout << endl;
 }
-Clock_deco();
     while(true){
-        cout << "Exit: 0" << endl << "Nombre: 1" << endl << "Temperatura: 2" << endl << "Distancia: 3" << endl << "+/-: 4" << endl << "Interseccion: 5" << endl << "Mayor: 7" << endl;
+        cout << "Exit: 0" << endl << "Nombre: 1" << endl << "Temperatura: 2" << endl << "Distancia: 3" << endl << "+/-: 4" << endl << "Interseccion: 5" << endl << "Mayor: 7" << endl << "Desfasaje: 8" << endl;
         cin >> programa;
 switch(programa){
 case 0:
@@ -61,20 +73,31 @@ break;
 {
 case 1:
     cout << "Por favor ingrese su nombre" << endl;
+
     cin >> nombre;
-Clock_deco();
-    cout << "Hola, " << nombre << endl;
+Clock_deco(8);
+cout << "H";cout.flush();bs(3.5);
+cout << "e";cout.flush();bs(3.5);
+cout << "l";cout.flush();bs(3.5);
+cout << "l";cout.flush();bs(3.5);
+cout << "o";cout.flush();bs(3.5);
+cout << ",";cout.flush();bs(3.5);
+cout << " ";cout.flush();bs(3.5);
+for(string::size_type i2 = 0;nombre.length() > i2;i2++){
+    cout << nombre[i2];cout.flush();bs(3.5);
+}
+
+cout << endl;
 break;
 }
-//Programa Temperatura
+//Temperatura
 {
 case 2:
     cout << "Por favor ingrese la temperatura en fahrenheit" << endl;
-float temperaturaF, temperaturaC;
-    cin >> temperaturaF;
-    temperaturaC = 5.0/9 * (temperaturaF - 32);
-Clock_deco();
-    cout << "La temperatura en grados celsios es: Cº " << temperaturaC << endl;
+    int temperatura;
+    temperatura = Temperatura();
+Clock_deco(8);
+    cout << "La temperatura en grados celsios es: Cº " << temperatura << endl;
 break;
 }
 //Programa Distancia
@@ -85,7 +108,7 @@ case 3:
     cin >> KMH;
     cout << "Por favor ingrese el tiempo desde el lanzamiento en minutos" << endl;
     cin >> tiempo;
-Clock_deco();
+Clock_deco(8);
     cout << "Distancia recorrida: " << tiempo / 60 * KMH << " KM" << endl;
 
 break;
@@ -96,7 +119,7 @@ case 4:
     int numero;
     cout << "Ingrese un numero" << endl;
     cin >> numero;
-Clock_deco();
+Clock_deco(8);
     if (numero>0){
         cout << "El numero " << numero << " es positivo" << endl;
     }
@@ -117,7 +140,7 @@ case 5:
     cin >> pendiente1 >> pendiente2;
     cout << "Ingrese la ordenada de la primer recta seguido de la segunda" << endl;
     cin >> ordenada1 >> ordenada2;
-Clock_deco();
+Clock_deco(8);
     if (pendiente1==pendiente2){
         if (ordenada1==ordenada2){
             cout << "La solucion es toda la recta" << endl;
@@ -135,6 +158,16 @@ break;
 //Programa fecha
 {
 case 6:
+        cout << "Escriva una fecha" << endl;
+
+              cin.clear();cin.ignore(1);
+              getline (cin, fecha);
+                cin >> compare;
+                if(fecha[0] == compare[0]){
+                    fecha="AAAAA";
+                }
+
+                cout << fecha << compare << endl;
 break;
 }
 //Programa mayor valor
@@ -159,6 +192,44 @@ case 7:
     }else{
          cout << "Mayor valor: " << mayorvalor << endl << "Promedio: " << promedio/i << endl;
     }
+}
+//Programa cifrado Cesar! Discreta
+{
+case 8:
+    cout << "Bienvenido al programa encriptador v0.1" << endl << "Por favor, ingrese el desfasaje de caracteres" << endl;
+    cin >> desfasaje;
+    cout << "Por favor, ingrese el mensaje a cifrar" << endl;
+    cin.ignore();getline (cin, toCode);
+    for(;desfasaje > 26; desfasaje-=26);
+Clock_deco(8);
+cout << " ";
+for(string::size_type i3 = 0;toCode.length() > i3;i3++){
+    if (toCode[i3] <= 90 && toCode[i3] >= 65){
+         toCode[i3] += desfasaje;
+         if (toCode[i3] > 90){
+             toCode[i3]+=(-26);
+         }else{
+             if (toCode[i3] < 65){
+                 toCode[i3]+=26;
+             }
+         }
+    }
+    else{
+        if (toCode[i3] <= 122 && toCode[i3] >= 97){
+            toCode[i3] += desfasaje;
+            if (toCode[i3] > 122){
+                toCode[i3]+=(-26);
+            }else{
+                if (toCode[i3] < 97){
+                    toCode[i3]+=26;
+                }
+            }
+        }
+       }
+    cout << toCode[i3];cout.flush();bs(3.5);
+}
+cout << endl;
+break;
 }
 
     }
